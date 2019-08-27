6f401ace11c7f75dabdfaa870e257937b3a21c26
Pramochete <orientedresearch@gmail.com>
2019-08-27 19:33:36 +0545

    created permissions relating to user and created highlight

--- tutorial/snippets/views.py
+++ tutorial/snippets/views.py
@@ -5,6 +5,11 @@ from rest_framework import generics
 from rest_framework.response import Response
 from django.contrib.auth.models import User
 from rest_framework import permissions
+from snippets.permissions import IsOwnerOrReadOnly
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework.reverse import reverse
+from rest_framework import renderers
 
 # Create your views here.
 class SnippetList(generics.ListCreateAPIView):    
@@ -19,7 +24,7 @@ class SnippetList(generics.ListCreateAPIView):
 class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
-    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
 
 class UserList(generics.ListAPIView):
     queryset = User.objects.all()
@@ -28,3 +33,18 @@ class UserList(generics.ListAPIView):
 class UserDetail(generics.RetrieveAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+
+@api_view(['GET'])
+def api_root(request, format=None):
+    return Response({
+        'users':reverse('user-list', request=request, format=format),
+        'snippets':reverse('snippet-list', request=request, format=format)
+    })
+
+class SnippetHighlight(generics.GenericAPIView):
+    queryset = Snippet.objects.all()
+    renderer_classes = [renderers.StaticHTMLRenderer]
+
+    def get(self,request, *args, **kwargs):
+        snippet = self.get_object()
+        return Response(snippet.highlighted)
