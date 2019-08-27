76950e38cdd549cb9b9dd151e8f000d24f21ac54
Pramochete <orientedresearch@gmail.com>
2019-08-27 16:04:13 +0545

    generics classes

--- tutorial/snippets/views.py
+++ tutorial/snippets/views.py
@@ -1,34 +1,14 @@
 from snippets.models import Snippet
 from snippets.serializers import SnippetSerializer
 from django.http import Http404
-from rest_framework import mixins, generics
+from rest_framework import generics
 from rest_framework.response import Response
 
 # Create your views here.
-class SnippetList(mixins.ListModelMixin,
-                  mixins.CreateModelMixin,
-                  generics.GenericAPIView):    
+class SnippetList(generics.ListCreateAPIView):    
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
 
-    def get(self, request, *args, **kwargs):
-        return self.list(request, *args, **kwargs)
-
-    def post(self, request, *args, **kwargs):
-        return self.create(request, *args, **kwargs)
-
-class SnippetDetail(mixins.RetrieveModelMixin,
-                    mixins.UpdateModelMixin,
-                    mixins.DestroyModelMixin, 
-                    generics.GenericAPIView):
+class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
-
-    def get(self, request, *args, **kwargs):
-        return self.retrieve(self, *args, **kwargs)
-    
-    def put(self, request, *args, **kwargs):
-        return self.update(self, *args, **kwargs)
-    
-    def delete(self, request, *args, **kwargs):
-        return self.destroy(self, *args, **kwargs)
