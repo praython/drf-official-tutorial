8b6d352e3a4b69967e7fcc637a41550b857bdf93
Pramochete <orientedresearch@gmail.com>
2019-08-27 15:03:56 +0545

    Used ModelSerializers and api_view

--- tutorial/snippets/models.py
+++ tutorial/snippets/models.py
@@ -16,3 +16,6 @@ class Snippet(models.Model):
 
     class Meta:
         ordering = ['created']
+    
+    def __str__(self):
+        return self.title
--- tutorial/snippets/urls.py
+++ tutorial/snippets/urls.py
@@ -1,7 +1,16 @@
 from django.urls import path
 from snippets import views
+from rest_framework.urlpatterns import format_suffix_patterns
 
 urlpatterns = [
     path('snippets/', views.snippet_list),
     path('snippets/<int:pk>', views.snippet_detail),
 ]
+
+#Including format_suffix_patterns is an optional 
+# choice that provides a simple, DRY way to refer 
+# to a specific file format for a URL endpoint. 
+# It means our API will be able to handle URls such as 
+# http://example.com/api/items/4.json rather than 
+# just http://example.com/api/items/4.
+urlpatterns = format_suffix_patterns(urlpatterns)
